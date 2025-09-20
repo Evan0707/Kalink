3952332faa90061db0a3bd9e2716117e2ac87829
Evan0707 <evan.gery07@gmail.com>
2025-09-20 19:10:47 +0200

    Git init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
 import Image from "next/image";
+import logo from '../../public/logo.png'
+import screen from '../../public/AppScreen.png'
+import { ChartGraphSolid, Earth, MobileSignalFourSolid, RefreshSolid } from "@mynaui/icons-react";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+        <div className="grid">
+          <div className="box a">
+            <a href="/" style={{display:'flex',alignItems:'center'}}>
+              <Image src={logo} width={60} alt=""/>
+              <h3 style={{fontSize:30,marginLeft:10}}>Kalink</h3>
+            </a>
+            <h1>Track your eSIM plans and activate them in one click</h1>
+            <section>
+              <p>Easily manage all your eSIM plans from a single app. Track your usage in real time, monitor remaining data and duration, and activate your plans in just a few taps. Stay connected wherever you go, whether traveling, working remotely, or exploring new places, all without swapping physical SIM cards.</p>
+              <h2>Join the Nomad eSIM Revolution</h2>
+              <input type="text"  placeholder="Enter your Email"/>
+              <button>Join waitlist</button>
+            </section>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="box b">
+            <div>
+              <Image className="screenImg" src={screen} alt=""/>
+            </div>
+          </div>
+
+          <div style={{backgroundColor:'#E3E2F6'}} className="box c">
+            <ul  style={{display:'flex',alignItems:'center',flexWrap:'wrap'}}>
+              <li>
+                <MobileSignalFourSolid size={60}/>
+                <p className="title">Instant connection</p>
+                <p className="subtitle">Activez vos forfaits en un clic, sans carte SIM physique.</p>
+              </li>
+
+              <li>
+                <RefreshSolid size={60}/>
+                <p className="title">Instant connection</p>
+                <p className="subtitle">Activez vos forfaits en un clic, sans carte SIM physique.</p>
+              </li>
+
+              <li>
+                <ChartGraphSolid size={60}/>
+                <p className="title">Instant connection</p>
+                <p className="subtitle">Activez vos forfaits en un clic, sans carte SIM physique.</p>
+              </li>
+
+              <li>
+                <Earth size={60}/>
+                <p className="title">Instant connection</p>
+                <p className="subtitle">Activez vos forfaits en un clic, sans carte SIM physique.</p>
+              </li>
+            </ul>
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
